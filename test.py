30e8cd85971c17c724a4cbf1a6cbbae61789f821
leeboydmerly <lee.boyd@merly.ai>
2024-10-22 10:57:35 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ a = b = c = 1
 if a and b and c and a or not c and a:
   a = b + 1
 print (a)
-if a == b:
+if a == b and a == b and a == b:
   print(a)
   b = a
 print(b)
 if b == c:
   print(b)
-if a == b:
+if a == b and a == b and a == b:
   print(b)
