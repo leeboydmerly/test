05786500a6259d04a4ca383be1754554b1c601bc
Lee Boyd <lee.boyd@merly.ai>
2025-07-02 10:48:02 -0500

    test

---
+++ test4.py
@@ -0,0 +1,27 @@
+# a→var1, b→var2, c→var3, d→var4, zebra→var5, white_tensor→var6, black_tensor→var7, f→var8, g→var9
+
+var1 = var2 = var3 = var4 = 1
+if var1 and var2 and var3 and var1 or not var3 and var1:
+    var1 = var2 + 1
+print(var1)
+
+if var1 == var2 and var1 == var2 and var1 == var2:
+    print(var1)
+    var2 = var1
+print(var2)
+
+if var2 == var3:
+    print(var2)
+
+if var1 == var2 and var1 == var2 and var1 == var2:
+    print(var2)
+
+for _ in range(10):
+    if np.all(var2.var3.var4[:, :, :, :-1] > 240):
+        var8.var9(20, True)
+    if np.all(var2.var3.var4[:, :, :, :, :-1] > 240):
+        var8.var9(20, True)
+    if np.all(var5.var6.var7[:, -1] >= 240) or np.all(var2.var3.var4[:, :, :, :-1] > 240):
+        var8.var9(20, True)
+    else:
+        break
