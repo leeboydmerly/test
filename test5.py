9dee80b6afa75c67149f1a5e447937edf86d2137
Lee Boyd <lee.boyd@merly.ai>
2025-07-24 09:48:30 -0500

    test

--- test5.py
+++ test5.py
@@ -2,20 +2,3 @@ a = b = c = d = 1
 if a and b and c and a or not c and a:
   a = b + 1
 print (a)
-if a == b and a == b and a == b:
-  print(a)
-  b = a
-print(b)
-if b == c:
-  print(b)
-if a == b and a == b and a == b:
-  print(b)
-for _ in range(10):
-        if np.all(b.c.d[:, :, :, : -1] > 240):
-            f.g(20, True)
-        if np.all(b.c.d[:, :, :, :, : -1] > 240):
-            f.g(20, True)
-        if np.all(zebra.white_tensor.black_tensor[:, -1] >= 240) or np.all(b.c.d[:, :, :, : -1] > 240):
-            f.g(20, True)
-        else:
-            break
