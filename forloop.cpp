60f81ba0b78e766c9c5b337885b18ec5656cd716
Lee Boyd <lee.boyd@merly.ai>
2025-07-29 11:44:14 -0500

    for loop test

---
+++ forloop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int start = 0;
+    int text_len = 100;
+    int direction = 1;
+    int i;
+
+    for ( i = start; i >= 0 AND i < text_len; i += direction ) {
+        // placeholder body
+        std::cout << "i = " << i << std::endl;
+    }
+
+    return 0;
+}
