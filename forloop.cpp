c42a1942a2af6ba3f750861023c1e841d8dabeee
Lee Boyd <lee.boyd@merly.ai>
2025-08-11 14:36:18 -0500

    test

--- forloop.cpp
+++ forloop.cpp
@@ -6,10 +6,19 @@ int main() {
     int direction = 1;
     int i;
 
-    for ( i = start; i >= 0 AND i < text_len; i += direction ) {
+    for ( i = start; i >= 0 && i < text_len; i += direction ) {
         // placeholder body
         std::cout << "i = " << i << std::endl;
     }
+    
+        if (!(OPENSSL_isalpha(c) || ((c == ' ')) || (c >= '\'' && c <= ':') ||
+          c == '=' || c == '?')){
+          return false;  
+        }
+        
+        if (((((a == b)) && (b == c) && (c == d) && (d == e) && (e == f) && (f == g)))) {
+          return false;
+        }
 
     return 0;
 }
