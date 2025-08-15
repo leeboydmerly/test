69c0c12ae3df871f5a7c0d43d70fde00d298163c
Lee Boyd <lee.boyd@merly.ai>
2025-08-15 10:06:46 -0500

    test

--- test3.py
+++ test3.py
@@ -1,3 +1,15 @@
 a = b = c = d = 1 
-if b:
-  print(a)
+
+if b == c:
+  print(b)
+if a == b and a == b and a == b:
+  print(b)
+for _ in range(10):
+        if np.all(b.c.d[:, :, :, : -1] > 240):
+            f.g(20, True)
+        if np.all(b.c.d[:, :, :, :, : -1] > 240):
+            f.g(20, True)
+        if np.all(zebra.white_tensor.black_tensor[:, -1] >= 240) or np.all(b.c.d[:, :, :, : -1] > 240):
+            f.g(20, True)
+        else:
+            break
